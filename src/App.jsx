0d30d0849afb56939a783eb48d8f668ecb3f5642
westonludeke <westonludeke@gmail.com>
2023-11-14 15:39:36 -0600

    create useField custom hook and add to /src/hooks/index.js file. update App.jsx to use useField hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useField } from './hooks/index';
 import { 
   BrowserRouter as Router,
   Routes, Route, Link, 
@@ -68,20 +69,20 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
-    props.setNotification(`A new anecdote ${content} created!`);
+    props.setNotification(`A new anecdote ${content.value} created!`);
     navigate('/');
   };
 
@@ -91,21 +92,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
     </div>
   );
-
 };
 
 const App = () => {
